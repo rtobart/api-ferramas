ee210e38de68106eb3ea7b7ca118653e90fbc3e3
Robinson.tobar <robinson.tobar.ti@gmail.com>
2024-05-05 19:34:28 -0400

    global module common services

--- src/common/services/services.module.ts
+++ src/common/services/services.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { CentralBankService } from './CentralBank.service';
 import { HttpCustomService } from './httpCustom.service';
 import { HttpModule } from '@nestjs/axios';
 
 const SERVICES = [CentralBankService, HttpCustomService];
 
+@Global()
 @Module({
   imports: [ HttpModule ],
   providers: [ ...SERVICES ],
